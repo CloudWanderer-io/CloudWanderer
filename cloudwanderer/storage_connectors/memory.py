92aa97254cc15fc677ed33171f0d75ca1ba0b501
Sam Martin <samjackmartin@gmail.com>
2020-12-24 10:09:12 +0000

    Functional Memory Storage Connector.

--- cloudwanderer/storage_connectors/memory.py
+++ cloudwanderer/storage_connectors/memory.py
@@ -1,3 +1,4 @@
+"""Storage connector to place data in memory."""
 from collections import defaultdict
 from typing import List
 import boto3
@@ -7,8 +8,13 @@ from ..cloud_wanderer import CloudWandererResource
 
 
 class MemoryStorageConnector(BaseStorageConnector):
+    """Storage connector to place data in memory.
 
-    def __init__(self):
+    Useful for testing.
+    """
+
+    def __init__(self) -> None:
+        """Initialise MemoryStorageConnector."""
         self._data = defaultdict(dict)
 
     def read_resource(self, urn: AwsUrn) -> List['CloudWandererResource']:
@@ -17,19 +23,65 @@ class MemoryStorageConnector(BaseStorageConnector):
         Arguments:
             urn (cloudwanderer.aws_urn.AwsUrn): The AWS URN of the resource to return
         """
-        yield from memory_item_to_resource(urn, self._data[str(urn)])
+        yield memory_item_to_resource(urn, self._data[str(urn)])
 
-    def read_all(self):
-        pass
+    def read_all(self) -> dict:
+        """Return the raw dictionaries stored in memory."""
+        for urn_str, items in self._data.items():
+            for item_type, item in items.items():
+                yield {
+                    **{
+                        'urn': urn_str,
+                        'attr': item_type,
+                    },
+                    **item
+                }
 
-    def read_all_resources_in_account(self):
-        pass
+    def read_all_resources_in_account(self, account_id: str) -> List['CloudWandererResource']:
+        """Return all resources in account.
 
-    def read_resource_of_type(self):
-        pass
+        Args:
+            account_id (str): AWS Account ID
+        """
+        for urn_str, items in self._data.items():
+            urn = AwsUrn.from_string(urn_str)
+            if urn.account_id != account_id:
+                continue
+            yield memory_item_to_resource(urn, items)
 
-    def read_resource_of_type_in_account(self):
-        pass
+    def read_resource_of_type(self, service: str, resource_type: str) -> List['CloudWandererResource']:
+        """Return all resources of type.
+
+        Args:
+            service (str): Service name (e.g. ``'ec2'``)
+            resource_type (str): Resource Type (e.g. ``'instance'``)
+        """
+        for urn_str, items in self._data.items():
+            urn = AwsUrn.from_string(urn_str)
+            if urn.service != service:
+                continue
+            if urn.resource_type != resource_type:
+                continue
+            yield memory_item_to_resource(urn, items)
+
+    def read_resource_of_type_in_account(
+            self, service: str, resource_type: str, account_id: str) -> List['CloudWandererResource']:
+        """Return all resources of the specified type in the specified AWS account.
+
+        Args:
+            service (str): Service name, e.g. ``'ec2'``
+            resource_type (str): Resource type, e.g. ``'instance'``
+            account_id (str): AWS Account ID
+        """
+        for urn_str, items in self._data.items():
+            urn = AwsUrn.from_string(urn_str)
+            if urn.service != service:
+                continue
+            if urn.account_id != account_id:
+                continue
+            if urn.resource_type != resource_type:
+                continue
+            yield memory_item_to_resource(urn, items)
 
     def write_resource(self, urn: AwsUrn, resource: boto3.resources.base.ServiceResource) -> None:
         """Write the specified resource to memory.
@@ -40,18 +92,39 @@ class MemoryStorageConnector(BaseStorageConnector):
         """
         self._data[str(urn)]['BaseResource'] = resource.meta.data
 
-    def delete_resource(self):
-        pass
+    def delete_resource(self, urn: AwsUrn) -> None:
+        """Delete the resource and all its resource attributes from DynamoDB."""
+        try:
+            del self._data[str(urn)]
+        except KeyError:
+            pass
 
-    def delete_resource_of_type_in_account_region(self):
-        pass
+    def delete_resource_of_type_in_account_region(
+            self, service: str, resource_type: str, account_id: str, region: str, urns_to_keep: AwsUrn = None) -> None:
+        """Delete resources of type in account id unless in list of URNs."""
+        urns_to_delete = []
+        for urn_str, items in self._data.items():
+            urn = AwsUrn.from_string(urn_str)
+            if urn.service != service:
+                continue
+            if urn.account_id != account_id:
+                continue
+            if urn.resource_type != resource_type:
+                continue
+            if urn.region != region:
+                continue
+            if urn in urns_to_keep:
+                continue
+            urns_to_delete.append(urn)
+        for urn in urns_to_delete:
+            del self._data[str(urn)]
 
 
 def memory_item_to_resource(urn: AwsUrn, items: dict) -> CloudWandererResource:
     """Convert a resource and its attributes to a CloudWandererResource."""
     attributes = [attribute for item_type, attribute in items.items() if item_type != 'BaseResource']
     base_resource = next(iter(resource for item_type, resource in items.items() if item_type == 'BaseResource'))
-    yield CloudWandererResource(
+    return CloudWandererResource(
         urn=urn,
         resource_data=base_resource,
         resource_attributes=attributes
