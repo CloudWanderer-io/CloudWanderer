c31ff85ba8fd6326dd4c106da134af8c0188af15
Sam Martin <samjackmartin@gmail.com>
2021-01-01 15:57:55 +0000

    Fixed sphinx extension

--- doc_source/_ext/supported_resources.py
+++ doc_source/_ext/supported_resources.py
@@ -1,3 +1,4 @@
+import logging
 from docutils import nodes
 from sphinx.util.docutils import SphinxDirective
 from sphinx.domains import Domain
@@ -30,7 +31,6 @@ class Boto3ResourcesDirective(SphinxDirective):
         service_list = nodes.bullet_list()
         boto3_services = self.boto3_interface._get_available_services()
         for service_name in boto3_services:
-
             resource_list = nodes.bullet_list()
             resource_collections = self.boto3_interface.get_resource_collections(
                 self.boto3_interface.get_boto3_resource_service(service_name)
@@ -51,12 +51,23 @@ class CloudWandererResourcesDirective(SphinxDirective):
         self.boto3_interface = cloudwanderer.boto3_interface.CloudWandererBoto3Interface(
             boto3_session=boto3.Session(region_name='eu-west-2')
         )
+        self.boto3_services = list(self.get_boto3_default_services())
 
     def run(self) -> list:
         targetid = 'cloudwanderer-%d' % self.env.new_serialno('cloudwanderer')
         targetnode = nodes.target('', '', ids=[targetid])
 
         return [targetnode, self.get_cloudwanderer_resources()]
+
+    def get_boto3_default_services(self):
+        boto3_services = self.boto3_interface._get_available_services()
+        for service_name in boto3_services:
+            resource_list = nodes.bullet_list()
+            resource_collections = self.boto3_interface.get_resource_collections(
+                self.boto3_interface.get_boto3_resource_service(service_name)
+            )
+            for resource_collection in resource_collections:
+                yield (service_name, resource_collection.name)
 
     def get_cloudwanderer_resources(self) -> list:
         service_list = nodes.bullet_list()
@@ -68,8 +79,10 @@ class CloudWandererResourcesDirective(SphinxDirective):
                 service
             )
             for resource_collection in resource_collections:
-                resource_list += nodes.list_item('', nodes.Text(capitalize_snake_case(resource_collection.name)))
-            service_list += nodes.list_item('', nodes.Text(capitalize_snake_case(service_name)), resource_list)
+                if (service_name, resource_collection.name) not in self.boto3_services:
+                    resource_list += nodes.list_item('', nodes.Text(capitalize_snake_case(resource_collection.name)))
+            if resource_list.children:
+                service_list += nodes.list_item('', nodes.Text(capitalize_snake_case(service_name)), resource_list)
         return service_list
 
 
@@ -80,8 +93,9 @@ class CloudWandererResourceAttributesDirective(SphinxDirective):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.secondary_attributes_interface = cloudwanderer.boto3_interface.SecondaryAttributesInterface(
-            boto3.Session(region_name='eu-west-1'))
+        self.boto3_interface = cloudwanderer.boto3_interface.CloudWandererBoto3Interface(
+            boto3_session=boto3.Session(region_name='eu-west-2')
+        )
 
     def run(self) -> list:
         targetid = 'cloudwanderer-%d' % self.env.new_serialno('cloudwanderer')
@@ -91,16 +105,14 @@ class CloudWandererResourceAttributesDirective(SphinxDirective):
 
     def get_cloudwanderer_secondary_attributes(self) -> list:
         service_list = nodes.bullet_list()
-        services = self.secondary_attributes_interface.custom_secondary_attribute_definitions.definitions
-        for service_name, service in services.items():
 
-            resource_list = nodes.bullet_list()
-            resource_collections = self.secondary_attributes_interface.get_resource_collections(
-                service
-            )
-            for resource_collection in resource_collections:
-                resource_list += nodes.list_item('', nodes.Text(resource_collection.name))
-            service_list += nodes.list_item('', nodes.Text(capitalize_snake_case(service_name)), resource_list)
+        for boto3_service in self.boto3_interface.get_all_custom_resource_services():
+            for collection in self.boto3_interface.get_resource_collections(boto3_service):
+                resource_list = nodes.bullet_list()
+                for secondary_attribute in self.boto3_interface.get_secondary_attribute_definitions(collection.resource.model):
+                    resource_list += nodes.list_item('', nodes.Text(secondary_attribute.name))
+                if resource_list.children:
+                    service_list += nodes.list_item('', nodes.Text(capitalize_snake_case(collection.name)), resource_list)
         return service_list
 
 
