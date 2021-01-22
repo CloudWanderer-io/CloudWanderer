75e8caf43fc0460de21e1d098d4afe06fd023cfc
Sam Martin <samjackmartin@gmail.com>
2021-01-22 16:17:53 +0000

    Added support for discovering secretsmanager secrets

---
+++ tests/integration/custom_resources/test_secrets_manager.py
@@ -0,0 +1,42 @@
+import unittest
+import boto3
+from cloudwanderer import CloudWanderer
+from cloudwanderer.storage_connectors import MemoryStorageConnector
+import moto
+from ..helpers import clear_aws_credentials, mock_services, GenericAssertionHelpers
+
+
+class TestSecretsManagerResources(unittest.TestCase, GenericAssertionHelpers):
+
+    @classmethod
+    def setUpClass(cls):
+        mock_services()
+        moto.mock_secretsmanager().start()
+        clear_aws_credentials()
+
+    def setUp(self):
+        self.storage_connector = MemoryStorageConnector()
+        self.wanderer = CloudWanderer(
+            storage_connectors=[self.storage_connector]
+        )
+
+    def test_secrets(self):
+        secretsmanager = boto3.client('secretsmanager')
+        secretsmanager.create_secret(
+            Name='TestSecret',
+            SecretString='Ssshhh'
+        )
+
+        self.wanderer.write_resources_of_service_in_region(region_name='eu-west-2', service_name='secretsmanager')
+
+        self.assert_dictionary_overlap(self.storage_connector.read_all(), [
+            {
+                'Name': 'TestSecret',
+                'Description': None,
+                'KmsKeyId': None,
+                'RotationEnabled': False,
+                'RotationLambdaARN': None,
+                'RotationRules': {'AutomaticallyAfterDays': 0},
+                'Tags': [],
+            }
+        ])
