5b490033a66fe5825a3ad6235f6c517808254591
Sam Martin <samjackmartin@gmail.com>
2021-01-24 14:11:36 +0000

    Linting

--- cloudwanderer/boto3_interface.py
+++ cloudwanderer/boto3_interface.py
@@ -345,8 +345,13 @@ class CloudWandererBoto3Interface:
         else:
             yield from self.enabled_regions
 
-    def _clean_boto3_metadata(self, boto3_metadata):
-        """Remove unwanted keys from boto3 metadata dictionaries."""
+    def _clean_boto3_metadata(self, boto3_metadata: dict) -> dict:
+        """Remove unwanted keys from boto3 metadata dictionaries.
+
+        Arguments:
+            boto3_metadata (dict): The raw dictionary of metadata typically found in resource.meta.data
+        """
+        boto3_metadata = boto3_metadata or {}
         unwanted_keys = ['ResponseMetadata']
         for key in unwanted_keys:
             if key in boto3_metadata:
