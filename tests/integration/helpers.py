dd33f235e815fa2e687ddfc24b3db2adb5f447de
Sam Martin <samjackmartin@gmail.com>
2021-01-31 17:41:10 +0000

    Ensure list of mocked services gets reset after teardown

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -191,6 +191,7 @@ class SetupMocking():
     def stop_moto_services(self):
         for service in self.service_mocks.values():
             service.stop()
+        self.service_mocks = {}
 
     def start_limit_collections_list(self, restrict_collections):
         """Limit the boto3 resource collections we service to a subset we use for testing."""
