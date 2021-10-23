bd90638fdab3e031d3688dce0c7db4894801746f
Sam Martin <samjackmartin@gmail.com>
2021-10-23 13:45:31 +0100

    Fixed boto3 helpers test

--- tests/integration/aws_interface/test_boto3_helpers.py
+++ tests/integration/aws_interface/test_boto3_helpers.py
@@ -1,27 +1,12 @@
-import unittest
-
 from cloudwanderer.aws_interface.boto3_helpers import _clean_boto3_metadata
 
-from ..helpers import get_default_mocker
-from ..mocks import add_infra
 
+def test__clean_boto3_metadata():
+    result = _clean_boto3_metadata(
+        boto3_metadata={
+            "ShouldIBeHere": "Yes",
+            "ResponseMetadata": {"ShouldIBeHere": "No"},
+        }
+    )
 
-class TestBoto3Helpers(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        get_default_mocker().start_general_mock()
-        add_infra()
-
-    @classmethod
-    def tearDownClass(cls):
-        get_default_mocker().stop_general_mock()
-
-    def test__clean_boto3_metadata(self):
-        result = _clean_boto3_metadata(
-            boto3_metadata={
-                "ShouldIBeHere": "Yes",
-                "ResponseMetadata": {"ShouldIBeHere": "No"},
-            }
-        )
-
-        assert result == {"ShouldIBeHere": "Yes"}
+    assert result == {"ShouldIBeHere": "Yes"}
