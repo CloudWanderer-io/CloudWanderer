d4f5c45592c5f02fa15c5f2ab44ae3db021a84b6
Sam Martin <samjackmartin@gmail.com>
2021-10-24 17:35:02 +0100

    Cleanup

--- tests/integration/storage_dynamodb/test_default.py
+++ tests/integration/storage_dynamodb/test_default.py
@@ -1,18 +1,22 @@
 import unittest
 
-from cloudwanderer.storage_connectors import DynamoDbConnector
+import boto3
 
-from ...helpers import DEFAULT_SESSION
+from cloudwanderer.storage_connectors import DynamoDbConnector
 
 
 class TestDynamoDBConnector(unittest.TestCase):
     def test_repr(self):
-        connector = DynamoDbConnector(boto3_session=DEFAULT_SESSION)
+        connector = DynamoDbConnector(
+            boto3_session=boto3.Session(aws_access_key_id="1", aws_secret_access_key="1", region_name="eu-west-2")
+        )
         assert repr(connector) == (
             'DynamoDbConnector(table_name="cloud_wanderer", endpoint_url="None", '
             'boto3_session="Session(region_name=\'eu-west-2\')", client_args="{}, number_of_shards=10)'
         )
 
     def test_str(self):
-        connector = DynamoDbConnector(boto3_session=DEFAULT_SESSION)
+        connector = DynamoDbConnector(
+            boto3_session=boto3.Session(aws_access_key_id="1", aws_secret_access_key="1", region_name="eu-west-2")
+        )
         assert str(connector) == "<DynamoDbConnector=cloud_wanderer>"
